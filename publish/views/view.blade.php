ffa5149be9d0205ab724d4d95fcdbdb80309e4f8
Unknown <cerny.vil@gmail.com>
2018-03-13 20:32:58 +0100

    update

--- publish/views/view.blade.php
+++ publish/views/view.blade.php
@@ -5,23 +5,52 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Admin setup</title>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
+		<style>
+			input[type="checkbox"]{
+				-webkit-transform: scale(1.5); /* Safari and Chrome */
+				margin-right: 15px;
+			}
+			ul li {
+			  list-style-type: none;
+			}
+
+		</style>
 	</head>
 	<body>
 		<div id="root">
-			<admin-section label="Database">
-				<admin-link route="drop-all-tables" >Drop all tables</admin-link>
-			</admin-section>
+			<section class="section">
+				<div class="container">
 
-			<admin-section label="Migrations">
-				<admin-link route="migrate" >Run migrations</admin-link>
-			</admin-section>
+					<div class="columns is-mobile">
+						<div class="column is-6">
+							<list ref="tables" get="get-tables">Tables</list>
+						</div>
+						<div class="column is-6">
+							<list ref="seeders" get="get-seeders">Seeders</list>
+						</div>
+					</div> 
 
-			<admin-section label="Seeding">
-				<admin-link route="seed" >Seed all tables</admin-link>
-				<list get="get-seeders" post="seed">Seed table</list>
-				<list get="get-tables"  post="seed-generate">Generate seed</list>
-			</admin-section>
+					<admin-section label="Tables">
+						<admin-link route="drop-all-tables" >Drop all tables</admin-link>
+						<admin-link route="truncate-tables" list="tables" >Truncate selected tables</admin-link>
+					</admin-section>
 
+					<admin-section label="Migrations">
+						<admin-link route="migrate" >Run migrations</admin-link>
+					</admin-section>
+
+					<admin-section label="Seeding">
+						<admin-link route="seed" >Seed all tables</admin-link>
+						<admin-link route="seed" list="seeders" >Seed selected Seeders</admin-link>
+						<admin-link route="seed-generate" list="tables" >Generate seeds for selected tables</admin-link>
+					</admin-section>
+
+					<admin-section label="Cache">
+						<admin-link route="cache-clear" >Clear cache</admin-link>
+					</admin-section>
+
+				</div>
+			</section>
 		</div>
 	</body>
 	<script src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
@@ -31,12 +60,12 @@
 
 		Vue.component('admin-section', {
 			props:['label'],
-			template: '<section><h2 class="title is-2 has-text-centered" v-html="label"></h2><slot></slot></section>',
+			template: '<section><h2 class="title is-2" v-html="label"></h2><slot></slot></section>',
 		})
 
 		Vue.component('list', {
-			props:['get','post'],
-			template: '<div><a href="#" @click.prevent="postList"  target="_blank" ><slot></slot></a><li v-for="item in items"><input type="checkbox" @change="toggleItem(item)" >@{{item}}</li></div>',
+			props:['get'],
+			template: '<ul><h5 class="title is-5 has-text-centered"><slot></slot></h5><li click="toggleItem(item)" v-for="item in items"><input type="checkbox" @change="toggleItem(item)" :id="item"><label :for="item">@{{item}}</label></li></ul>',
 			data(){
 				return {
 					items:[],
@@ -53,28 +82,25 @@
 					else
 						this.selected.splice(this.selected.indexOf(item), 1)
 				},
-				postList(){
-					var answer = confirm ('Do You want ' + event.target.innerHTML +' ?\n\n    '+ this.selected.join('\n    ') );
-					if (answer)
-						window.open( '/admin-setup/'+this.post+'/'+this.selected.join(), "_blank")
-					//alert( this.selected.join() );
-				}
-
 			 }
-
 		})
 
 		Vue.component('admin-link', {
-			props:['route'],
-			template: '<li><a href="#" :data-route="route" @click.prevent="confirmLink"  target="_blank" ><slot></slot></a></li>',
+			props:['route','list'],
+			template: '<li><a href="#" :data-route="route" :data-list="list" @click.prevent="confirmLink"  target="_blank" ><slot></slot></a></li>',
 			methods:{
+				getSelectedItems(list)
+				{
+					return list && this.$root.$refs[list].selected.length > 0 ? this.$root.$refs[list].selected : [];
+				},
 				confirmLink(){
-					var answer = confirm ('Do You want\n\n    ' + event.target.innerHTML + ' ?');
+					var selected_items	= this.getSelectedItems( event.target.dataset.list );
+					var answer	= confirm ('Do You want\n\n    ' + event.target.innerHTML + ' ?' + ( selected_items.length > 0 ? '\n\n    '+selected_items.join('\n    ') : '' ) );
 					if (answer)
-						window.open( '/admin-setup/'+event.target.dataset.route, "_blank")
+						window.open( '/admin-setup/'+event.target.dataset.route + ( selected_items.length > 0 ? '/'+selected_items.join() : '' ) , "_blank")
+
 				}
 			},
-
 		})
 
 		var app = new Vue({
@@ -83,6 +109,3 @@
 	</script>
 
 </html>
-
-
-
