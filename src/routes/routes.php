f513ab02ad9603a1f9d50d8837c09d111e8ef104
Unknown <cerny.vil@gmail.com>
2018-03-13 20:34:59 +0100

    update

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -17,70 +17,9 @@ Route::get('/admin-setup/get-tables', 'Vilbur\AdminSetup\Controllers\AdminSetupC
 Route::get('/admin-setup/seed-generate/{tables}', 'Vilbur\AdminSetup\Controllers\AdminSetupController@seeedGenerate' );
 
 
-/**
-* ROUTE ONLY FOR DEVELOPER
-**/
-//Route::get('/start','Backend\StartController@getView' );
-/**
-* ROUTE ONLY FOR START, no loggin needed
-//**/
-//Route::get('start', function(){
-//	//dump(  \Config::get('models') );
-//	//dump('start');exit();
-//	return view('backend.start', ["models" => \Config::get('models')]);
-//});
+Route::get('/admin-setup/cache-clear', function(){
+	\Artisan::call('cache:clear');
+});
 
 
-//dump('test');exit();
-/**
-* MIGRATE ALL TABLES
-**/
-//Route::get('/db/migrate','Backend\StartController@migrateAll' );
-
-/**
-* reset all id in table
-**/
-//Route::get('/db/resetid/{parent_model}/{model}','Backend\StartController@resetid' );
-//Route::get('/db/resetid/{parent_model}/{model}', [
-//	'as' => 'resetid',
-//	'middleware' => 'level:1',
-//	'uses' => 'Backend\StartController@resetid',
-//]);
-
-/**
-* SEED ALL TABLES
-**/
-
-//Route::get('/db/seed','Backend\StartController@seedAll' );
-
-/**
-* CREATE SEED FILES OF ALL TABLES
-**/
-//Route::get('/db/seed/create/all','Backend\StartController@createAllSeedFiles' );
-
-/**
-* DROP ALL DB TABLES
-**/
-//Route::get('/db/delete','Backend\StartController@deleteAllTables' );
-
-///**
-//* CLEAR CACHE
-//**/
-//Route::get('/cache/clear', function(){
-//	\Artisan::call('cache:clear');
-//});
-
-/**
-* SEED SPECIFIC TABLE
-**/
-//Route::get('/db/seedTable/{seederClass}', 'Backend\StartController@seedTable');
-/**
-* CREATE SEED FILE OF SPECIFIC TABLE
-**/
-//Route::get('/db/createSeedFile/{model}', 'Backend\StartController@createSeedFile');
-/**
-* TRUNCATE SPECIFIC TABLE
-**/
-//Route::get('/db/truncateTable/{model}','Backend\StartController@truncateTable' );
-
-//Route::get('/db/database/create/{migration}', 'Backend\StartController@runSingleMigration');
+Route::get('/admin-setup/truncate-tables/{tables}', 'Vilbur\AdminSetup\Controllers\AdminSetupController@truncateTables' );
